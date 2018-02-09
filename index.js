0f121ffc94221deb0af6a90768a1090c33241acb
Fredrik <fredrik.christenson@gmail.com>
2018-02-09 22:15:31 +0100

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports = function Option(object) {
+  const handler = {
+    _savedProp: undefined,
+
+    _missingProps: [],
+
+    $get: function() {
+      if (typeof this._savedProp !== "function") return this._savedProp;
+      return this._savedProp(...arguments);
+    },
+
+    _isObject: function(prop) {
+      return typeof prop === "object" && !Array.isArray(prop);
+    },
+
+    _hasMissingProp: function(prop) {
+      return prop === undefined;
+    },
+
+    get: function(target, prop) {
+      const val = target[prop];
+      console.log(prop);
+      console.log("--------------------------");
+      if (prop === "$get") return this.$get.bind(target);
+      if (prop === "$undefined") return this._missingProps;
+      if (this._hasMissingProp(val)) this._missingProps.push(prop);
+
+      this._savedProp = val;
+
+      if (this._isObject(val)) return new Proxy(val, this);
+
+      return new Proxy(this, this);
+    }
+  };
+
+  return new Proxy(object, handler);
+};
---
+++ test/object.spec.js
@@ -0,0 +1,7 @@
+const Option = require("../");
+
+describe("Object test", () => {
+  it("has a module", () => {
+    expect(Option).toBeDefined();
+  });
+});
